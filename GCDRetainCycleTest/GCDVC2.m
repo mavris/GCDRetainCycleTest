1932802af6d75d889785609205ad9572487401e0
Michael Mavris <michaelmavris@gmail.com>
2016-07-29 22:29:31 +0300

    Final

--- GCDRetainCycleTest/GCDVC2.m
+++ GCDRetainCycleTest/GCDVC2.m
@@ -19,32 +19,64 @@ typedef void(^CustomBlock)(void);
 
 @implementation GCDVC2
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.proArray = [[NSMutableArray alloc]init];
     
-    GCDVC2* __weak weakSelf = self;
-
     self.postGCDBlock = ^{
-
-        GCDVC2* __strong strongSelf = weakSelf;
-
-        [strongSelf.proArray removeObject:@"3"];
-        NSLog(@"%@",strongSelf.proArray);
-        [strongSelf.activityIndicator stopAnimating];
+        
+        [self.proArray removeObject:@"3"]; //At this line Xcode will warn you that this line is likely to lead on a retain cycle
+        NSLog(@"%@",self.proArray);
+        [self.activityIndicator stopAnimating];
     };
     
     self.addObjectsBlock = ^{
         
-        GCDVC2* __strong strongSelf = weakSelf;
-        
-        [strongSelf.proArray addObject:@"2"];
+        [self.proArray addObject:@"2"]; //At this line Xcode will warn you that this line is likely to lead on a retain cycle
+        [self.proArray addObject:@"3"];
         [NSThread sleepForTimeInterval:10];
         
-        dispatch_async(dispatch_get_main_queue(),strongSelf.postGCDBlock);
+        dispatch_async(dispatch_get_main_queue(),self.postGCDBlock);
     };
 }
+
+
+//The final solution without retain cycle
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    // Do any additional setup after loading the view.
+//    self.proArray = [[NSMutableArray alloc]init];
+//    
+//    GCDVC2* __weak weakSelf = self;
+//
+//    
+//    self.postGCDBlock = ^{
+//        
+//        GCDVC2* __strong strongSelf = weakSelf;
+//
+//        [strongSelf.proArray removeObject:@"3"];
+//        NSLog(@"%@",strongSelf.proArray);
+//        [strongSelf.activityIndicator stopAnimating];
+//    };
+//    
+//    self.addObjectsBlock = ^{
+//        
+//        GCDVC2* __strong strongSelf = weakSelf;
+//        
+//        [strongSelf.proArray addObject:@"2"];
+//        [strongSelf.proArray addObject:@"3"];
+//        [NSThread sleepForTimeInterval:5];
+//        
+//        dispatch_async(dispatch_get_main_queue(),^{
+//            strongSelf.postGCDBlock();
+//        });
+//
+//    };
+//    
+//}
+
 
 - (IBAction)startGCDRetainCycle:(id)sender {
     
